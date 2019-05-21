4e8404176ac6dff0ef65110c7f7df6cfd9fa80a7
Jinoo Lee <etture@gmail.com>
2019-05-22 01:32:07 +0900

    Cleaned up code

--- src/views/pages/AuthPage.tsx
+++ src/views/pages/AuthPage.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { inject, observer } from 'mobx-react';
 import { ITodoStore } from '../../stores/TodoStore';
 import { withRouter, RouteComponentProps, Switch, Route } from 'react-router-dom';
 import SigninPage from './SigninPage';
@@ -8,24 +7,11 @@ import SignupPage from './SignupPage';
 interface IAuthPageProps extends RouteComponentProps<{}> {
 	todoStore?: ITodoStore
 }
-interface IAuthPageState {
-	nickname: string,
-	password: string
-}
+interface IAuthPageState { }
 
-@inject('todoStore')
-@observer
 class AuthPage extends Component<IAuthPageProps, IAuthPageState> {
 	constructor(props: IAuthPageProps) {
 		super(props);
-		this.state = {
-			nickname: '',
-			password: ''
-		};
-	}
-
-	componentDidMount() {
-		// this.props.history.push('/auth/signin');
 	}
 
 	render() {
